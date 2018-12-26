a97a87a15bba3ed1dc6ae09f8bc284f9a66c82d6
Clement <menthos_c@live.fr>
2018-12-26 15:43:11 +0100

    Modifs classes model

--- codenames-model/src/main/java/codenames/model/Administrateur.java
+++ codenames-model/src/main/java/codenames/model/Administrateur.java
@@ -1,5 +1,13 @@
 package codenames.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "administrateur")
+@PrimaryKeyJoinColumn(name = "ADM_ID", referencedColumnName = "UTI_ID")
 public class Administrateur extends Utilisateur {
 
+	
+	
+	
 }
--- codenames-model/src/main/java/codenames/model/Joueur.java
+++ codenames-model/src/main/java/codenames/model/Joueur.java
@@ -1,8 +1,29 @@
 package codenames.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name="joueur")
+@PrimaryKeyJoinColumn(name= "JOU_ID", referencedColumnName= "UTI_ID")
 public class Joueur extends Utilisateur {
+	
+	
+	@NotEmpty
+	@NotNull
+	@Column(name = "JOU_PSEUDO")
 	private String pseudo ;
+	
+	@Column(name = "JOU_BANNI")
 	private boolean banni;
+	
+	
+	
+	
 	public String getPseudo() {
 		return pseudo;
 	}
@@ -16,10 +37,21 @@ public class Joueur extends Utilisateur {
 		this.banni = banni;
 	}
 	
-	public static Joueur creerJoueur(String pseudo, boolean banni) {
-		Joueur j = new Joueur() ;
-		j.setBanni(banni) ;
-		j.setPseudo(pseudo) ;
-		return j ;
-	}
+	
+	
+	
+	
+	
+	
+	
+//	public static Joueur creerJoueur(String pseudo, boolean banni) {
+//		Joueur j = new Joueur() ;
+//		j.setBanni(banni) ;
+//		j.setPseudo(pseudo) ;
+//		return j ;
+//	}
+	
+	
+	
+	
 }
