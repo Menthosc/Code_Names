7d5463f83d40c23ce6af982c53ea2465b1ef7481
Yohan44 <yhnf@protonmail.com>
2018-12-26 16:53:50 +0100

    Création des DAO Utilisateur, Partie et Carte

---
+++ codenames_dao/src/main/java/codenames/dao/JPA/DAOCarteJPA.java
@@ -0,0 +1,49 @@
+package codenames.dao.JPA;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import codenames.model.Carte;
+import codenames_dao.IDAOCarte;
+
+public class DAOCarteJPA implements IDAOCarte{
+	EntityManager em ;
+	
+	
+	public DAOCarteJPA(EntityManagerFactory emf) {
+		em = emf.createEntityManager();
+	}
+	
+	public List<Carte> findAll() {
+		List<Carte> mesCartes = em.createQuery("select c from Carte c", Carte.class).getResultList();
+		return mesCartes;
+	}
+	
+	public Carte findById(int id) {
+		return em.find(Carte.class, id);
+	}
+	
+	public Carte save(Carte c) {
+		EntityTransaction tx = em.getTransaction(); 
+		tx.begin();
+		if (c.getId() == 0) {
+			em.persist(c);
+		}
+		else {
+			return em.merge(c);
+		}
+		tx.commit(); 
+		return c ;
+	}
+	
+	public void delete (Carte c) {
+		
+	}
+	
+	public void deleteById (int id) {
+		
+	}
+}
---
+++ codenames_dao/src/main/java/codenames/dao/JPA/DAOUtilisateurJPA.java
@@ -0,0 +1,49 @@
+package codenames.dao.JPA;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+
+import codenames.model.Utilisateur;
+import codenames_dao.IDAOUtilisateur;
+
+public class DAOUtilisateurJPA implements IDAOUtilisateur {
+EntityManager em ;
+	
+	public DAOUtilisateurJPA(EntityManagerFactory emf) {
+		em = emf.createEntityManager();
+	}
+	
+	public List<Utilisateur> findAll() {
+		List<Utilisateur> mesUtilisateurs = em.createQuery("select u from Utilisateur u", Utilisateur.class).getResultList();
+		return mesUtilisateurs;
+	}
+	
+	public Utilisateur findById(int id) {
+		return em.find(Utilisateur.class, id);
+	}
+	
+	public Utilisateur save(Utilisateur u) {
+		EntityTransaction tx = em.getTransaction(); 
+		tx.begin();
+		if (u.getId() == 0) {
+			em.persist(u);
+		}
+		else {
+			return em.merge(u);
+		}
+		tx.commit(); 
+		return u ;
+	}
+	
+	public void delete (Utilisateur u) {
+		
+	}
+	
+	public void deleteById (int id) {
+		
+	}
+}
