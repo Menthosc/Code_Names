9776d3332bcd8c0b76e03982ebca04148b5dab25
Yohan44 <yhnf@protonmail.com>
2019-01-16 12:26:32 +0100

    Maj connexion + menu

--- codenames-web/WebContent/connexion.js
+++ codenames-web/WebContent/connexion.js
@@ -1,45 +1,58 @@
 /**
  * Gère la connexion de l'utilisateur
  */
-if (isConnected == true){
-	$('section').hide();
-	$('section #connecte').show();
+
+function Utilisateur(username, password){	
 }
 
-else {
-	$('section').hide();
-	$('section #pasConnecte').show();
+$('section').hide();
+$('section#pasConnecte').show();
 
-// Récupère les données du serveur concernant les comptes des utilisateurs
+var isConnected = 0;
+var monUtilisateur = new Utilisateur;
+var monTableau = [];
+
+//Récupère les données du serveur concernant les comptes des utilisateurs
 	$.ajax({
 		method: 'GET',
-		url: '',
+		url: 'http://192.168.1.110/codenames-ajax/utilisateur',
 		contentType: "application/json",
 		success: function(monTableauUtilisateur) {
 			console.log(monTableauUtilisateur);
 			for (let utilisateur of monTableauUtilisateur){
-				verifConnexion(utilisateur) ;
+				monTableau.push(utilisateur);
 			}
 		}
 	});
 	
-	// Récupère les données rentrées par l'utilisateur qui se connecte au moment où celui-ci clique sur Valider
+// Récupère les données rentrées par l'utilisateur qui se connecte au moment où celui-ci clique sur Valider
 	function connexion(utilisateur){
-		var monUtilisateur ={
-				username: $('input #identifiant').val(),
-				password: $('input #password').val(),
+		monUtilisateur ={
+				username: $('input#identifiant').val(),
+				password: $('input#password').val(),
+		}
+		console.log(monUtilisateur);
+		
+		for (let utilisateur of monTableau){
+			verifConnexion(utilisateur, monUtilisateur) ;
+			
+			if (isConnected == 0){
+				console.log("Identifiant ou mot de passe incorrect - Veuillez reessayer");
+				$('section').hide();
+				$('section#mauvaiseConnexion').show();
+			}
+		}
 	}
 	
-	// Compare les données rentrées par l'utilisateur qui cherchent à se connecter 
-	// et les données des utilisateurs de la base de données
-	function verifConnexion(utilisateur) {
+// Compare les données rentrées par l'utilisateur qui cherchent à se connecter 
+// et les données des utilisateurs de la base de données
+	function verifConnexion(utilisateur, monUtilisateur) {
 		if (utilisateur.username == monUtilisateur.username && utilisateur.password == monUtilisateur.password ) {
-			isConnected = true ;
-			return "Vous êtes connecté";
-		}
-		else {
-			return "Identifiant ou mot de passe incorrect - Réessayer";
+			console.log("Vous etes connecte");
+			isConnected = 1;
+			$('section').hide();
+			$('section#connecte').show();
+			return "Vous êtes connecte";
 		}
 	};
-	}	
-}
+		
