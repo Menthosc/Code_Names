4d45004f2c8d299eebf35bd2c0cb1117b99f7f3b
Yohan44 <yhnf@protonmail.com>
2019-01-17 15:47:19 +0100

    Chat

--- codenames-web/WebContent/jeu.js
+++ codenames-web/WebContent/jeu.js
@@ -110,7 +110,25 @@ function shuffle(array) {
 }
 
 
-// Pour révéler les cartes
+// Pour le chat
+
+(function() {
+
+	$('#live-chat header').on('click', function() {
+
+		$('.chat').slideToggle(300, 'swing');
+		$('.chat-message-counter').fadeToggle(300, 'swing');
+
+	});
+
+	$('.chat-close').on('click', function(e) {
+
+		e.preventDefault();
+		$('#live-chat').fadeOut(300);
+
+	});
+
+}) ();
 
 
 
