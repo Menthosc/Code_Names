dd7899f381a65f192c7d0519851b86a3f6cc2a0d
Yohan44 <yhnf@protonmail.com>
2019-01-18 09:02:53 +0100

    Maj

--- codenames-web/WebContent/jeu.js
+++ codenames-web/WebContent/jeu.js
@@ -181,25 +181,8 @@ function envoi(event){
 			maDiv.append(monJoueur);
 			maDiv.append(monSpan);
 			$('div#chatHist').append(maDivVide);
-			$('span').append(monSpanHeure);
 			$('div#chatHist').append(maDiv);
+			$('div#chatHist').append(monSpanHeure);
 	  }
 
-
-window.onload=function() {
-	  horloge('div_horloge');
-	};
-	 
-	function horloge(el) {
-	  if(typeof el=="string") { el = document.getElementById(el); }
-	  function actualiser() {
-	    var date = new Date();
-	    var str = date.getHours();
-	    str += ':'+(date.getMinutes()<10?'0':'')+date.getMinutes();
-	    str += ':'+(date.getSeconds()<10?'0':'')+date.getSeconds();
-	    el.innerHTML = str;
-	  }
-	  actualiser();
-	  setInterval(actualiser,1000);
-	}
 }
