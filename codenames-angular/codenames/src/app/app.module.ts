82cdc2a5974dcaa1057f9083eca82e39ae2fc958
Yohan44 <yhnf@protonmail.com>
2019-02-04 16:20:36 +0100

    Maj

--- codenames-angular/codenames/src/app/app.module.ts
+++ codenames-angular/codenames/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CaseComponent } from './case/case.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaseComponent
   ],
   imports: [
     BrowserModule
