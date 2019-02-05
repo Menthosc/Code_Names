a4f6572c94b707d9a4e0d0a7e91b1e37b8a12804
Clement <menthos_c@live.fr>
2019-02-05 15:00:48 +0100

    .

--- codenames-angular/codenames/src/app/jeu.service.ts
+++ codenames-angular/codenames/src/app/jeu.service.ts
@@ -68,29 +68,6 @@ export class JeuService {
 
 
 
-    reveler(){
-      var cCase: String =$(this).find('span').text(); //récupère le mot de la carte dans la case cliquée
-    	var that: any = $(this);
-      var couleur: String = cCase.couleur ;
-      this.httpClient.post("http://localhost:8080/api/jeu", cCase, this.httpOptions).subscribe();
-
-      var texte: String = $(that).find('span').text();
-
-      var monTexte: String = $('<h5> Le joueur a cliqué sur </h5>');
-
-      monTexte.append(texte);
-      $('section#infos').append(monTexte);
-
-      var monTexte2: String = $('<h5> Et la couleur est... : </h5>');
-      monTexte2.append(couleur);
-      $('section#infos').append(monTexte2);
-
-      var couleurNoire: String = "NOIRE" ;
-      if (couleur == couleurNoire){
-       var monTexte3 = $('<h5> Dommage... vous avez perdu </h5>');
-       $('section#infos').append(monTexte3);
-      }
-
 
     reveler(c:Case){
 
